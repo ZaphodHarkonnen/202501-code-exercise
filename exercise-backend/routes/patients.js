dd8ba6ab74e271b41de0837cee8ce97614c407ee
James Sullivan <James@tfg.nz>
2025-01-21 01:47:52 +1300

    Working API for record persistance.
    Missing backend validation.
    Contains notes on what I would tackle next.

--- exercise-backend/routes/patients.js
+++ exercise-backend/routes/patients.js
@@ -10,8 +10,11 @@ async function main() {
     await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 }
 
+// Need to examine if placing in here is the appropriate pattern to use.
+// It feels that the schema should be recorded elsewhere to make reuse in the solution simpler.
 const patientModelSchema = new mongoose.Schema({
     id: "string",
+    fullName: "string",
     dateOfBirth: "date",
     contact: {
         email: "string",
@@ -21,56 +24,35 @@ const patientModelSchema = new mongoose.Schema({
     notes: "string"
 });
 
-const patientModel = mongoose.model("PatientModel", patientModelSchema);
+const Patient = mongoose.model("Patient", patientModelSchema);
 
-function getPatients() {
-    // Call into mongo and get all current patients
-    return [];
-}
+// Next steps are digging into ExpressJS functionality and finding out how much API documentation can be generated for consumers. 
 
-router.get('/', (req, res) => {
-    res.send(getPatients({
-        id: "",
-        fullName: "",
-        dateOfBirth: new Date(),
-        contact: {
-            email: "",
-            phoneNumber: ""
-        },
-        diagnosisStatus: "",
-        notes: ""
-    }));
+router.get('/', async (_, res) => {
+    var patients = await Patient.find({ id: { $exists: true } }).exec();
+    res.send(patients);
 })
 
-function getPatient(id) {
-    return {
-        id: "",
-        fullName: "",
-        dateOfBirth: new Date(),
-        contact: {
-            email: "",
-            phoneNumber: ""
-        },
-        diagnosisStatus: "",
-        notes: ""
-    }
-}
-
-router.get('/:id', (req, res) => {
-    res.send(getPatient(req.params.id))
+router.get('/:id', async (req, res) => {
+    let patient = await Patient.findOne({ id: req.params.id }).exec();
+    res.send(patient)
 })
-
 
 router.post('/:id', async (req, res) => {
-    // updatePatient(req.params.id, req.body)
-    let newPatient = new patientModel(req.body);
+    let newPatient = new Patient(req.body);
     await newPatient.save();
-    res.send(req.body)
+    res.status(200).end();
 })
 
-router.delete('/:id', (req, res) => {
-    // deletePatient(req.params.id);
-    res.status(204).end();
+router.put('/:id', async (req, res) => {
+    const patient = new Patient(req.body);
+    await Patient.findByIdAndUpdate(req.params._id, patient).exec();
+    res.status(200).end();
+})
+
+router.delete('/:id', async (req, res) => {
+    await Patient.deleteOne({ id: req.params.id }).exec();
+    res.status(200).end();
 })
 
 module.exports = router;
