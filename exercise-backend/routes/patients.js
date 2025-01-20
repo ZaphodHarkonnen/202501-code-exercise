4083b028abd754893104c0c96e71892dcd69598f
James Sullivan <James@tfg.nz>
2025-01-20 19:11:40 +1300

    A few tweaks to get mongoose hooked up.
    Restored two of the template files to understand why a change wasn't working. Found I had used the wrong syntax in the adding a route method. Leaving in for now. Will clean unused files out as the last step.

--- exercise-backend/routes/patients.js
+++ exercise-backend/routes/patients.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+main().catch((err) => console.log(err));
+async function main() {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+}
+
+const patientModelSchema = new mongoose.Schema({
+    id: "string",
+    dateOfBirth: "date",
+    contact: {
+        email: "string",
+        phone: "string"
+    },
+    diagnosisStatus: "string",
+    notes: "string"
+});
+
+const patientModel = mongoose.model("PatientModel", patientModelSchema);
+
 function getPatients() {
     // Call into mongo and get all current patients
     return [];
 }
 
 router.get('/', (req, res) => {
-    res.send(getPatients());
+    res.send(getPatients({
+        id: "",
+        fullName: "",
+        dateOfBirth: new Date(),
+        contact: {
+            email: "",
+            phoneNumber: ""
+        },
+        diagnosisStatus: "",
+        notes: ""
+    }));
 })
 
 function getPatient(id) {
@@ -21,27 +53,24 @@ function getPatient(id) {
         },
         diagnosisStatus: "",
         notes: ""
-    };
+    }
 }
 
 router.get('/:id', (req, res) => {
     res.send(getPatient(req.params.id))
 })
 
-function updatePatient(id, body) {
-    return body;
-}
 
-router.post('/:id', (req, res) => {
-    updatePatient(req.params.id, req.body)
+router.post('/:id', async (req, res) => {
+    // updatePatient(req.params.id, req.body)
+    let newPatient = new patientModel(req.body);
+    await newPatient.save();
+    res.send(req.body)
 })
-
-function deletePatient(id) {
-
-    return true;
-}
 
 router.delete('/:id', (req, res) => {
-    deletePatient(req.params.id);
+    // deletePatient(req.params.id);
     res.status(204).end();
 })
+
+module.exports = router;
